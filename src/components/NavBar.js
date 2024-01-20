5031c3a7cf1fc5938f44f628fab4ed558d602da1
Ruth Lim <>
2024-01-20 16:31:11 +0800

    Add navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 export default function NavBar() {
-  return (
-    <>
-    </>
-  );
+    return (
+      <header className="header">
+        <div className="top-bar">
+          <Link to="/">
+            <img
+              src="logo.svg"
+              alt="LocalLinker Logo"
+              className="logo"
+            />
+          </Link>
+          <nav className="nav">
+            <Link to="/about" className="nav-link">About</Link>
+            <Link to="/listings" className="nav-link">Listings</Link>
+            <Link to="/contact" className="nav-link">Contact</Link>
+          </nav>
+          <Link to="/chatbot" className="get-started-button">Get Started</Link>
+        </div>
+      </header>
+    );
 }
